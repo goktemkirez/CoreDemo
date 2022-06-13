4b4119223bb7ee65938a21cbc78765b3b2bcbb25
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-13 16:07:30 +0300

    Lesson 31

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,25 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    Username = "Orçun"
+                },
+                new UserComment {
+                    ID = 2,
+                    Username = "Göktem"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
