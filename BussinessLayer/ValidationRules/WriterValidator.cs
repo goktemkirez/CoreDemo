50780daee05535ac2bd743b494c37fb90e443fb7
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-16 18:36:13 +0300

    Lesson 36

--- BussinessLayer/ValidationRules/WriterValidator.cs
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -14,13 +14,15 @@ namespace BussinessLayer.ValidationRules
         {
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail boş geçilemez");
+            RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Geçerli bir mail adresi giriniz!");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("En az 2 karakter giriniz.");
             RuleFor(x => x.WriterPassword).Matches("[A-Z]").WithMessage("Şifre büyük harf içermelidir.");
+            RuleFor(x => x.WriterPassword).Matches("[0-9]").WithMessage("Şifre rakam içermelidir.");
 
-    //            RuleFor(x => x.ConfirmPassword)
-    //.Equal(x => x.Password)
-    //.WithMessage("Passwords do not match");
+            //RuleFor(x => x.ConfirmPassword)
+            //.Equal(x => x.Password)
+            //.WithMessage("Passwords do not match");
         }
     }
 }
