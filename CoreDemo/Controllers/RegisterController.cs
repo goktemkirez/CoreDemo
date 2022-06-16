67f109661ce0cd1aeab3d0b68191ae553709b375
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-16 13:15:24 +0300

    Lesson 33

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+
+        }
+    }
+}
