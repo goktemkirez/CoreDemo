810c79e693706a0d9c981657784a1541ab6737fc
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-22 19:07:00 +0300

    Lesson 44

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    [Authorize]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+    }
+}
