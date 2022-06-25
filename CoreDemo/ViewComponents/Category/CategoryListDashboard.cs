33f1b68f0ec2a1b4e57db0363d375ae34f7791fe
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-25 16:39:42 +0300

    Lesson 63

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
