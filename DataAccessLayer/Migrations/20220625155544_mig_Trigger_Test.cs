4e381f22994e4027f343d7caa5df914c491fad26
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-06-25 19:02:15 +0300

    Added triggers to sql with code first

---
+++ DataAccessLayer/Migrations/20220625155544_mig_Trigger_Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Trigger_Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE TRIGGER AddBlogInRatingTable
+ON Blogs
+AFTER INSERT
+AS
+DECLARE @ID INT
+SELECT @ID=BlogID FROM inserted
+INSERT INTO BlogRatings (BlogID, BlogTotalScore, BlogRatingCount)
+VALUES (@ID, 0, 0)
+GO
+
+CREATE TRIGGER AddScoreInComment
+ON Comments
+AFTER INSERT
+AS
+DECLARE @ID INT
+DECLARE @SCORE INT
+DECLARE @RATING_COUNT INT
+SELECT @ID = BlogID, @SCORE = BlogScore FROM inserted
+UPDATE BlogRatings SET BlogTotalScore = BlogTotalScore + @SCORE, BlogRatingCount+=1 
+WHERE BlogID = @ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
