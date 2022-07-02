685b84d0d26d1e5a5207daa5b88fcaa7f7c4d0ca
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-07-02 12:29:35 +0300

    Lesson 103 - Weather API and XML usage

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,16 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string temperatureCity = "İstanbul";
+            string apiKey = "647a4fab9e55da2d1041b2b1cd6ea317";
+            string apiConnection = "https://api.openweathermap.org/data/2.5/weather?q=" + 
+                temperatureCity + "&mode=xml&" +
+                "lang=tr&units=metric&appid=" + apiKey;
+            XDocument document = XDocument.Load(apiConnection);
+            ViewBag.tempCity = temperatureCity;
+            ViewBag.temp = document.Descendants("temperature").ElementAt(0)
+                .Attributes("value").FirstOrDefault().Value;
             return View();
         }
     }
