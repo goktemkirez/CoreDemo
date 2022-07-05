082b2098bcb072cc106c6ed51e9150c0cffb283b
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-07-05 18:46:27 +0300

    Lesson 114 - Added API

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("server=goktemkirez.database.windows.net,1433;database=TestDB;" +
+            //    "user id=goktemkirez; password=Pujz5lpvg1; " +
+            //    "integrated security=false;");
+
+            optionsBuilder.UseSqlServer("server=GOKTEMKIREZ-PC;database=CoreBlogApiDb; " +
+                "integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
