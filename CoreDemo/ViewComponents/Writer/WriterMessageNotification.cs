e1ecce3943846d96c61a82b0a10656b5028c9550
Orçun Göktem Kirez <ogkirez@gmail.com>
2022-07-20 06:32:05 +0300

    Lesson 134

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BussinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +8,15 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterMessageNotification:ViewComponent
     {
         MessageManager mm = new MessageManager(new EfMessageRepository());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            var values = mm.GetInboxListByWriter(1);
+            var userName = User.Identity?.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName)
+                .Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID)
+                .FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
             return View(values);
         }
     }
