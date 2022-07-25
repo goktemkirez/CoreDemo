4885bd87b23c0a2ca4f3c2121c6c3618832ef1c8
goktemkirez <ogkirez@gmail.com>
2022-07-25 05:36:10 +0300

    Lesson 136

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,41 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageRepository());
+        Context c = new Context();
+
+        public IActionResult Inbox()
+        {
+            var userName = User.Identity?.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName)
+                .Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID)
+                .FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity?.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName)
+                .Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID)
+                .FirstOrDefault();
+            var values = mm.GetSendBoxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult ComposeMessage()
+        {
+            return View();
+        }
+    }
+}
