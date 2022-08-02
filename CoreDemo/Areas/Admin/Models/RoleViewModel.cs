4eecca765978f08b5190fa19fda567b8768e3975
goktemkirez <ogkirez@gmail.com>
2022-08-02 15:56:32 +0300

    Lesson 139 - Rol Listeleme ve ekleme

---
+++ CoreDemo/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = "Rol adı giriniz")]
+        public string name { get; set; }
+    }
+}
