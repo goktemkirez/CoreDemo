8edee5db1f8af78aa43c42bfc52efbe5d4b49ee8
goktemkirez <ogkirez@gmail.com>
2022-08-02 16:47:33 +0300

    Lesson 141

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -9,10 +9,12 @@ namespace CoreDemo.Areas.Admin.Controllers
     public class AdminRoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AdminRoleController(RoleManager<AppRole> roleManager)
+        public AdminRoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -85,6 +87,35 @@ namespace CoreDemo.Areas.Admin.Controllers
             if (result.Succeeded)
                 return RedirectToAction("Index");
             return View(result);
+        }
+
+        public IActionResult UserRoleList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+
+            TempData["UserId"] = user.Id;
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignViewModel> model = new List<RoleAssignViewModel>();
+            foreach(var item in roles)
+            {
+                RoleAssignViewModel m = new RoleAssignViewModel();
+                m.RoleID = item.Id;
+                m.Name = item.Name;
+                m.Exist = userRoles.Contains(item.Name);
+                model.Add(m);
+            }
+
+            return View(model);
         }
     }
 }
